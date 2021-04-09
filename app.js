eb15e3461ea849178de1489604412be34c91b924
Fabiola Montoya M <75088452+fabimmu@users.noreply.github.com>
2021-04-08 21:04:36 -0600

    Sidebar Imagen

---
+++ app.js
@@ -0,0 +1,8 @@
+//Mostrar sidebars con cada boton
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
